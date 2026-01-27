ba8c5b7158e785231ed4d606150dab01af3644f8
Ahmed Elhadary <ahmedelhadary.cs@gmail.com>
2026-01-27 20:03:33 +0200

    Customizing the homepage

--- Mobile-Temp/js/home.js
+++ Mobile-Temp/js/home.js
@@ -1,10 +1,15 @@
 $(function () {
   "use strict";
+
+  /* ===============================
+     1. Home Banner Swiper
+  =============================== */
   const bannerSwiper = new Swiper("[data-swiper='banner']", {
     slidesPerView: 1,
     spaceBetween: 15,
     loop: true,
     speed: 700,
+    rtl: true,
     lazy: {
       loadPrevNext: true,
       enabled: true,
@@ -21,59 +26,60 @@ $(function () {
     grabCursor: true,
   });
 
+
+  /* ===============================
+     2. Terms Swiper
+  =============================== */
   const termsSwiper = new Swiper("[data-swiper='terms']", {
     slidesPerView: 1.5,
     spaceBetween: 20,
     speed: 700,
     grabCursor: true,
+    rtl: true,
     lazy: {
       loadPrevNext: true,
       enabled: true,
     },
     autoplay: {
       delay: 3000,
-      reverseDirection: true,
       disableOnInteraction: false,
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".terms-next",
+      prevEl: ".terms-prev",
     },
     breakpoints: {
-      576: {
-        slidesPerView: 2.5,
-      },
-      768: {
-        slidesPerView: 4,
-      },
+      576: { slidesPerView: 2.5 },
+      768: { slidesPerView: 4 },
     },
   });
 
+
+  /* ===============================
+     3. Courses Swiper
+  =============================== */
   const coursesSwiper = new Swiper("[data-swiper='courses']", {
     slidesPerView: 2.25,
     spaceBetween: 15,
     speed: 700,
     grabCursor: true,
-    autoplay: {
-      delay: 5000,
-      reverseDirection: true,
-      disableOnInteraction: false,
-    },
+    rtl: true,
     lazy: {
       loadPrevNext: true,
       enabled: true,
     },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".courses-next",
+      prevEl: ".courses-prev",
     },
     breakpoints: {
-      576: {
-        slidesPerView: 3.25,
-      },
-      768: {
-        slidesPerView: 4.25,
-      },
+      576: { slidesPerView: 3.25 },
+      768: { slidesPerView: 4.25 },
     },
   });
+
 });
